60491df41c074f59ca56a15e172361617111c37e
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2021-11-11 12:13:28 -0700

    aboutmobile y homemobile listos

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -3,21 +3,24 @@ import 'package:provider/provider.dart';
 import 'package:ramses_indalecio_web/providers/page_provider.dart';
 
 import 'package:ramses_indalecio_web/ui/shared/custom_app_menu.dart';
+import 'package:ramses_indalecio_web/ui/views/about_mobile_view.dart';
 
 import 'package:ramses_indalecio_web/ui/views/about_view.dart';
 import 'package:ramses_indalecio_web/ui/views/call_of_duty_view.dart';
 import 'package:ramses_indalecio_web/ui/views/contacto_view.dart';
 import 'package:ramses_indalecio_web/ui/views/home_view.dart';
+import 'package:ramses_indalecio_web/ui/views/home_view_mobile.dart';
 import 'package:ramses_indalecio_web/ui/views/portafolio_view.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         child: Stack(
           children: [
-            _HommeBody(),
+            (size.width > 950) ? _HommeBody() : _MobileBody(),
             Positioned(right: 20, top: 20, child: CustomAppMenu())
           ],
         ),
@@ -47,5 +50,19 @@ class _HommeBody extends StatelessWidget {
     // Container(
     //   color: Colors.blue[800],
     // );
+  }
+}
+
+class _MobileBody extends StatelessWidget {
+  const _MobileBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final pageProvider = Provider.of<PageProvider>(context, listen: false);
+    return PageView(
+      controller: pageProvider.scrollcontroller,
+      scrollDirection: Axis.vertical,
+      children: [HomeMoileView(), AboutMobileView()],
+    );
   }
 }
